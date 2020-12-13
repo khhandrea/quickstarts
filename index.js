c28c9ed37edffce48a06f3cbead4301b12f981db
khhandrea <khhandrea@naver.com>
2020-12-13 15:37:28 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const body = document.querySelector("#body");
+
+function handleClick() {
+    if (body.className == "clicked") {
+        body.className = "";
+    } else {
+        body.className = "clicked";
+    }
+}
+
+function init() {
+    body.addEventListener("click", handleClick);
+}
+init();
