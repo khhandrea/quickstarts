c59c3d8bf4b63a4faae9988f75b9b77396fd785b
khhandrea <khhandrea@kakao.com>
2023-07-31 12:50:55 +0900

    feat: Add pomodoro app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_toonflix/stateless.dart';
-import 'package:flutter_toonflix/stateful.dart'; // StatefulApp()
-// import 'package:flutter_toonflix/pomodoro.dart'; // PomodoroApp()
+// import 'package:flutter_toonflix/stateless.dart'; //StatelessApp()
+// import 'package:flutter_toonflix/stateful.dart'; // StatefulApp()
+import 'package:flutter_toonflix/pomodoro.dart'; // PomodoroApp()
 
 void main() {
-  runApp(const StatefulApp());
+  // runApp(const StatelessApp());
+  // runApp(const StatefulApp());
+  runApp(const PomodoroApp());
 }
--- lib/pomodoro.dart
+++ lib/pomodoro.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_toonflix/screens/home_screen.dart';
 
-class PomodoroApp extends StatefulWidget {
+class PomodoroApp extends StatelessWidget {
   const PomodoroApp({super.key});
 
   @override
-  State<PomodoroApp> createState() => _PomodoroAppState();
-}
-
-class _PomodoroAppState extends State<PomodoroApp> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
+        textTheme: const TextTheme(
+            displayLarge: TextStyle(
+          color: Color(0xFF232B55),
+        )),
+        cardColor: const Color(0xFFF4EDDB),
+      ),
+      home: const HomeScreen(),
+    );
   }
 }
